4fe7f475c0f7794671d1cff9c52dac8e19351431
atcervantes <inge.cervantes@gmail.com>
2018-06-25 15:52:15 -0500

    Add files via upload

---
+++ CodingInterview/src/main/java/com/cervantes/Factorial.java
@@ -0,0 +1,21 @@
+package com.cervantes;
+
+/**
+ * Created by ancervan on 25/06/2018.
+ */
+public class Factorial {
+
+    public static int factorial(int n){
+
+        if(n==1)
+            return 1;
+
+        return n+factorial(n-1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+}
